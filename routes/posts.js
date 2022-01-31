f01c2415ee22515fca11771115c285115cc62d2a
SNUDerek <dsh9470@snu.ac.kr>
2022-01-31 19:24:41 +0900

    lowercase schema name

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Post = require('../models/Post')
+const Post = require('../models/post')
 
 // GET all posts, async
 router.get('/', async (req, res) => {
