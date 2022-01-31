88d8404146c1bfa7132d49841f696facdfe28141
SNUDerek <dsh9470@snu.ac.kr>
2022-01-31 19:18:48 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# REST API example with mongodb, node and express
+
+## requirements
+
+- `npm` + `node`
+- `docker` and (optionally) `docker-compose` for running `mongoDB`
+- (optional) `python` + `jupyter` for testing the api, else use Postman or VS Code "REST Client" extension
+
+## start
+
+```
+# start mongodb
+docker-compose up -d
+
+# install necessary packages
+npm install
+
+# run with nodemon
+npm start
+```
+
+## references
+
+based on:
+
+[Build A Restful Api With Node.js Express & MongoDB | Rest Api Tutorial](https://www.youtube.com/watch?v=vjf774RKrLc)  
+[Build A REST API With Node.js, Express, & MongoDB - Quick](https://www.youtube.com/watch?v=fgTGADljAeg)  
---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// routes
+const postsRouter = require('./routes/posts');
+app.use('/posts', postsRouter);
+
+app.get('/', (req, res) => {
+    res.send('we are at home');
+});
+
+app.get('/json', (req, res) => {
+    console.log('test middleware for json');
+    res.json({"hello": "world"});
+});
+
+// mongodb
+console.log(`mongo url: ${process.env.DB_URL}`)
+mongoose.connect(process.env.DB_URL, () => console.log('connected to database'));
+const db = mongoose.connection;
+db.on('error', (err) => console.error(err));
+
+// start listening
+const appPort = process.env.API_PORT || 6969;
+app.listen(appPort, () => console.log(`server started on port ${appPort}`));
